d8ad116d75dc188d79784faf2d07e45a6f2564b0
Bhaskar <bhaskar9.koilada@gmail.com>
2021-04-13 17:01:17 +0530

    promises in node

---
+++ node/promises/fetchAPIdata/promisesInNode/index.js
@@ -0,0 +1,33 @@
+const fs = require("fs").promises
+// fs.readFile("./sample.txt","utf-8",function(data){
+//     console.log(data)
+// })
+
+// fs.writeFile("write.txt","hey bhaskar writing file read",function(data)
+// {
+//     console.log(data)
+// })
+
+// fs.writeFile("new","hiiiiiiiiiiiiiiii").then(()=> console.log("done"))
+
+// fs.writeFile("test","iam testing").then(()=>console.log("dones"))
+
+
+async function read(){
+    const data = await fs.readFile("asyn.txt","utf-8")
+    console.log(data)
+}
+
+read()
+
+
+async function writ(){
+    try {
+
+    const data = await fs.writeFile("hey.txt","iam writing data in hey file")
+    console.log(data)
+} catch (error){
+
+}
+
+writ()
